98ed8efda8811a0d0d828d6e50111a6926a2b2d6
Oey Darryl Valencio Wijaya <Darryl16888@gmail.com>
2022-12-18 21:52:22 +0700

    user type panti

--- resources/views/components/kursus/tutor/detail-tutor/card.blade.php
+++ resources/views/components/kursus/tutor/detail-tutor/card.blade.php
@@ -66,7 +66,7 @@
                     </svg>
                     <p class="text-lg leading-8 text-gray-500">{{ $surel }}</p>
                 </div>
-                @if (Auth::user()->user_type == 'tutor')
+                @if (Auth::user()->user_type == 'Pengurus Panti')
                 <div class="mt-2">
                     <button type="button"
                         class="w-full inline-flex justify-center items-center space-x-2 rounded focus:outline-none px-3 py-2 leading-6 bg-blue-500 hover:bg-blue-600 focus:ring focus:ring-blue-500 focus:ring-opacity-50 active:bg-blue-500 active:border-blue-500">
