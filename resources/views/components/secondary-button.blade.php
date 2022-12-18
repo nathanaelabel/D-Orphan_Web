288f7f2e7989e1a4958a1fd0294e3a8a046a0257
Probo Krishnacahya <probo.krishnacahya@gmail.com>
2022-12-19 00:49:59 +0700

    page lomba

--- resources/views/components/secondary-button.blade.php
+++ resources/views/components/secondary-button.blade.php
@@ -1,4 +1,4 @@
 <button type="button"
-    class="inline-flex justify-center items-center space-x-2 rounded focus:outline-none px-3 py-2 leading-6 bg-blue-100 hover:bg-blue-200 focus:ring focus:ring-blue-100 focus:ring-opacity-50 active:bg-blue-100 active:border-blue-100">
+    class="w-full inline-flex justify-center items-center space-x-2 rounded focus:outline-none px-3 py-2 leading-6 bg-blue-100 hover:bg-blue-200 focus:ring focus:ring-blue-100 focus:ring-opacity-50 active:bg-blue-100 active:border-blue-100">
     <p class="font-semibold text-blue-700">{{ $slot }}</p>
 </button>
