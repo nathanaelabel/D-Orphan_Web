8070371c0533fae78edd4b7fe4b804d3ff722853
Nur Azizah <nur.azizah.7241@gmail.com>
2022-12-19 11:41:28 +0700

    .

--- app/Http/Livewire/CompetitionRecommendation.php
+++ app/Http/Livewire/CompetitionRecommendation.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\CompetitionRecommendation as ModelsCompetitionRecommendation;
 use Livewire\Component;
 
 class CompetitionRecommendation extends Component
 {
+    public $competitionRecommendation;
+
     public function render()
     {
         return view('livewire.competition-recommendation');
+    }
+
+    public function mount()
+    {
+        //  $this->competitionRecommendation = ModelsCompetitionRecommendation::where('orphanage_id', auth()->user()->orphanage->id)->get();
     }
 }
