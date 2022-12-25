a2703754dbda21891418f5494e8eda76bfe58279
Probo Krishnacahya <probo.krishnacahya@gmail.com>
2022-12-25 19:53:09 +0700

    kelola kursus yang disediakan tutor

--- resources/views/livewire/course-category.blade.php
+++ resources/views/livewire/course-category.blade.php
@@ -1,7 +1,11 @@
 <div class="space-y-8">
     @if (auth()->user()->tutor)
         {{-- Title --}}
-        <h3 class="text-3xl leading-10 font-bold">{{ 'Jelajahi kursus' }}</h3>
+        @if ($activeTabCourseCategory == 'eksplor')
+            <h3 class="text-3xl leading-10 font-bold">{{ 'Jelajahi kursus yang tersedia' }}</h3>
+        @else
+            <h3 class="text-3xl leading-10 font-bold">{{ 'Atur kursus yang Anda sediakan' }}</h3>
+        @endif
         {{-- Tabs --}}
         <div class="flex justify-center">
             <nav class="text-center bg-blue-100 rounded space-x-4 px-2 py-4" aria-label="Tabs">
@@ -68,24 +72,84 @@
 @endif
 
 @if (auth()->user()->tutor && $activeTabCourseCategory == 'kelola')
-    <div>
-        @if ($coursesTutor->count())
-            <div class="grid gap-2 border-2 border-gray-300 border-dashed rounded p-2 place-items-center">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                    stroke="currentColor" class="w-24 h-24">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                        d="M3.75 9.776c.112-.017.227-.026.344-.026h15.812c.117 0 .232.009.344.026m-16.5 0a2.25 2.25 0 00-1.883 2.542l.857 6a2.25 2.25 0 002.227 1.932H19.05a2.25 2.25 0 002.227-1.932l.857-6a2.25 2.25 0 00-1.883-2.542m-16.5 0V6A2.25 2.25 0 016 3.75h3.879a1.5 1.5 0 011.06.44l2.122 2.12a1.5 1.5 0 001.06.44H18A2.25 2.25 0 0120.25 9v.776" />
-                </svg>
-                <p>{{ 'Belum ada kursus yang disediakan' }}</p>
-            </div>
-        @else
-            <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-                @foreach ($coursesTutor as $item)
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque porro maxime sunt eius modi!
-                        Dolore itaque nostrum enim veniam, dignissimos illum sunt distinctio, neque architecto
-                        accusamus, culpa nobis voluptatum ex.</p>
-                @endforeach
-            </div>
-        @endif
+    <div class="overflow-x-auto shadow rounded">
+        <table class="min-w-full">
+            <thead class="bg-gray-500 text-white">
+                <tr>
+                    <th scope="col" class="sticky top-0 z-10 px-3 py-3.5 text-left font-semibold">
+                        Tarif per jam</th>
+                    <th scope="col" class="sticky top-0 z-10 px-3 py-3.5 text-left font-semibold">
+                        Batas peserta kursus</th>
+                    <th scope="col" class="sticky top-0 z-10 px-3 py-3.5 text-left font-semibold">
+                        Durasi kursus</th>
+                    <th scope="col" class="sticky top-0 z-10 px-3 py-3.5 text-left font-semibold">
+                        Tipe Kehadiran</th>
+                    <th scope="col" class="sticky top-0 z-10 px-3 py-3.5 text-left font-semibold">
+                        Lokasi</th>
+                    <th scope="col" class="sticky top-0 z-10 px-3 py-3.5 text-left font-semibold">
+                        Aksi</th>
+                </tr>
+            </thead>
+            @if ($coursesTutor->isEmpty())
+                <tbody class="bg-white">
+                    <tr>
+                        <td colspan="6" class="px-3 py-4">
+                            <div
+                                class="grid gap-2 border-2 border-gray-300 border-dashed rounded p-2 place-items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="currentColor" class="w-24 h-24">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M3.75 9.776c.112-.017.227-.026.344-.026h15.812c.117 0 .232.009.344.026m-16.5 0a2.25 2.25 0 00-1.883 2.542l.857 6a2.25 2.25 0 002.227 1.932H19.05a2.25 2.25 0 002.227-1.932l.857-6a2.25 2.25 0 00-1.883-2.542m-16.5 0V6A2.25 2.25 0 016 3.75h3.879a1.5 1.5 0 011.06.44l2.122 2.12a1.5 1.5 0 001.06.44H18A2.25 2.25 0 0120.25 9v.776" />
+                                </svg>
+                                <p>{{ 'Belum ada kursus yang disediakan' }}</p>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            @else
+                <tbody>
+                    @foreach ($coursesTutor as $item)
+                        <tr class="odd:bg-white even:bg-gray-100">
+                            <td class="whitespace-nowrap px-3 py-4">
+                                {{ 'Rp' . number_format($item->hourly_price, 2, ',', '.') }}
+                            </td>
+                            <td class="whitespace-nowrap px-3 py-4">
+                                {{ $item->maximum_member . ' anak/Panti' }}
+                            </td>
+                            <td class="whitespace-nowrap px-3 py-4">
+                                {{ $item->name }}
+                            </td>
+                            <td class="whitespace-nowrap px-3 py-4">
+                                @if ($item->is_online == 0)
+                                    {{ 'Luring' }}
+                                @elseif ($item->is_online == 1)
+                                    {{ 'Daring' }}
+                                @elseif ($item->is_visit == 0)
+                                    {{ 'Luring' }}
+                                @else
+                                    {{ '-' }}
+                                @endif
+                            </td>
+                            <td class="whitespace-nowrap px-3 py-4">
+                                @if ($item->location == null)
+                                    {{ '-' }}
+                                @else
+                                    {{ $item->location }}
+                                @endif
+                            </td>
+                            <td class="whitespace-nowrap px-3 py-4 flex gap-2">
+                                <a href="{{ route('/') }}" class="cursor-pointer text-blue-500" title="Detail">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+                                    </svg>
+                                </a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            @endif
+        </table>
     </div>
 @endif
