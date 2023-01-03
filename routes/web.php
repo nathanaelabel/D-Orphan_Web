19a95519301001f7673ea0fffc270533e053afcb
Nur Azizah <nur.azizah.7241@gmail.com>
2023-01-03 14:37:26 +0700

    cleaning

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,6 @@
 <?php
 
 use App\Http\Controllers\TransactionController;
-use App\Http\Controllers\CompetitionController;
-use App\Http\Controllers\CompetitionRecommendationController;
-use App\Http\Controllers\CourseBookingController;
-use App\Http\Controllers\CourseController;
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\OrphanageController;
-use App\Http\Controllers\OrphanCrController;
 use App\Http\Livewire\CompetitionManageAdmin;
 use App\Http\Livewire\CompetitionRecommendation;
 use App\Http\Livewire\CourseBooking;
@@ -43,29 +36,8 @@ Route::get('/donasi', function () {
 
 Route::get('/detail-user/{user_id}', DetailUser::class)->name('detail-user');
 Route::get('/kirim-donasi/{user_id}', DonationDelivery::class)->name('kirim-donasi');
-
-Route::get('/mydashboard', [DashboardController::class, 'index']);
-Route::get('/mydashboard/{status}/{courseBookingId}', [CourseBookingController::class, 'changeCourseBookingStatus']);
-
-Route::get('/getmytutorcoursecollection', [CourseController::class, 'getTutorCourseCollection']);
-Route::resource('/mytutorcoursecaollection', CourseController::class);
-
-Route::resource('/mytutorcompetition', CompetitionController::class);
-Route::get('/getmytutorcompetition/{competitionId}', [CompetitionRecommendationController::class, 'index']);
-Route::get('/mytutororphancrs/{competitionRecommendationId}', [OrphanCrController::class, 'show']);
-
-Route::resource('/myrequestsaldotutor', TransactionController::class);
-Route::get('/myrequestsaldotutor/{status}/{transactionId}', [TransactionController::class, 'changeTransactionStatus']);
-
-Route::get('/myadminrequestsaldotutor', [TransactionController::class, 'index']);
-
-Route::get('/myfindpaskill', [CourseController::class, 'getPaSkill']);
-Route::get('/myfindpaskill/{courseId}', [CourseController::class, 'show']);
-Route::get('/padata', [OrphanageController::class, 'index']);
-Route::get('/padata/{orphanage}', [OrphanageController::class, 'show']);
-Route::get('/competition/{competitionId}', [CompetitionController::class, 'show']);
 Route::get('midtrans/{id}', [TransactionController::class, 'showMidtrans'])->name('midtrans');
-//buat panti
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
