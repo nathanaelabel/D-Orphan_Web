f8932c1a3f40a4df3abcdf8f158a4ab75b27438d
Oey Darryl Valencio Wijaya <Darryl16888@gmail.com>
2023-01-09 10:51:26 +0700

    update

--- resources/views/livewire/course-booking.blade.php
+++ resources/views/livewire/course-booking.blade.php
@@ -102,36 +102,50 @@
             </div>
         </div>
     @else
-        <p> Pendapatan bulan ini :
-            {{ 'Rp' .number_format(App\Models\Transaction::whereIn('id',App\Models\CourseBooking::whereIn('course_id', App\Models\Course::where('tutor_id', Auth::user()->tutor->id)->pluck('id'))->where('status', 'complete')->pluck('transaction_id'))->whereMonth('updated_at', \Carbon\Carbon::now()->month)->sum('amount')) }}
-        </p>
-        <x-dasbor.card>
-            <x-slot:title>Kursus menunggu persetujuan</x-slot:title>
-            <x-slot:icon>
-                <path stroke-linecap="round" stroke-linejoin="round"
-                    d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
-            </x-slot:icon>
-            <x-slot:number>{{ App\Models\CourseBooking::whereIn('course_id', App\Models\Course::where('tutor_id', Auth::user()->tutor->id)->pluck('id'))->where('status','pending')->count() }}
-            </x-slot:number>
-        </x-dasbor.card>
-        <x-dasbor.card>
-            <x-slot:title>Kursus sedang berlangsung</x-slot:title>
-            <x-slot:icon>
-                <path stroke-linecap="round" stroke-linejoin="round"
-                    d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
-            </x-slot:icon>
-            <x-slot:number>{{ App\Models\CourseBooking::whereIn('course_id', App\Models\Course::where('tutor_id', Auth::user()->tutor->id)->pluck('id'))->where('status','ongoing')->count() }}
-            </x-slot:number>
-        </x-dasbor.card>
-        <x-dasbor.card>
-            <x-slot:title>Kursus telah diselesaikan</x-slot:title>
-            <x-slot:icon>
-                <path stroke-linecap="round" stroke-linejoin="round"
-                    d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
-            </x-slot:icon>
-            <x-slot:number>{{ App\Models\CourseBooking::whereIn('course_id', App\Models\Course::where('tutor_id', Auth::user()->tutor->id)->pluck('id'))->where('status','complete')->count() }}
-            </x-slot:number>
-        </x-dasbor.card>
+        <div class="flex flex-col space-y-4">
+            <div class="grid grid-cols-2 gap-4 md:grid-cols-2 lg:grid-cols-4">
+                <x-dasbor.card>
+                    <x-slot:title>Pendapatan bulan ini</x-slot:title>
+                    <x-slot:icon>
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25" />
+                    </x-slot:icon>
+                    <x-slot:number>
+                        {{ 'Rp' .number_format(App\Models\Transaction::whereIn('id',App\Models\CourseBooking::whereIn('course_id', App\Models\Course::where('tutor_id', Auth::user()->tutor->id)->pluck('id'))->where('status', 'complete')->pluck('transaction_id'))->whereMonth('updated_at', \Carbon\Carbon::now()->month)->sum('amount')) }}
+                    </x-slot:number>
+                </x-dasbor.card>
+                <x-dasbor.card>
+                    <x-slot:title>Kursus menunggu persetujuan</x-slot:title>
+                    <x-slot:icon>
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+                    </x-slot:icon>
+                    <x-slot:number>
+                        {{ App\Models\CourseBooking::whereIn('course_id', App\Models\Course::where('tutor_id', Auth::user()->tutor->id)->pluck('id'))->where('status', 'pending')->count() }}
+                    </x-slot:number>
+                </x-dasbor.card>
+                <x-dasbor.card>
+                    <x-slot:title>Kursus sedang berlangsung</x-slot:title>
+                    <x-slot:icon>
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+                    </x-slot:icon>
+                    <x-slot:number>
+                        {{ App\Models\CourseBooking::whereIn('course_id', App\Models\Course::where('tutor_id', Auth::user()->tutor->id)->pluck('id'))->where('status', 'ongoing')->count() }}
+                    </x-slot:number>
+                </x-dasbor.card>
+                <x-dasbor.card>
+                    <x-slot:title>Kursus telah diselesaikan</x-slot:title>
+                    <x-slot:icon>
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+                    </x-slot:icon>
+                    <x-slot:number>
+                        {{ App\Models\CourseBooking::whereIn('course_id', App\Models\Course::where('tutor_id', Auth::user()->tutor->id)->pluck('id'))->where('status', 'complete')->count() }}
+                    </x-slot:number>
+                </x-dasbor.card>
+            </div>
+        </div>
     @endif
 
     {{-- Tabs --}}
