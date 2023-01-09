ba45faac103b382de829ff7c6ab8b8289f8d07c0
galih <haning.grk@gmail.com>
2023-01-09 11:15:25 +0700

    a

--- app/Http/Controllers/PaymentCallbackController.php
+++ app/Http/Controllers/PaymentCallbackController.php
@@ -14,13 +14,21 @@ class PaymentCallbackController extends Controller
         $serverKey = config('midtrans.server_key');
         $hashed = hash("sha512",$request->order_id.$request->status_code.$request->gross_amount.$serverKey);
         if($hashed == $request->signature_key){
-            if($request->transaction_status=='capture'){
+            if($request->transaction_status=='capture' || $request->transaction_status=='settlement'){
                 $order = Transaction::find($request->order_id);
                 $order->update([
                     'status' => 'complete'
                 ]);
                 $order->user->increment('money', $order->amount);
+
+                return $order->user->money;
+            }else{
+                return "Gagal";
             }
+         
+        }else{
+            return "Signature Key Salah";
         }
+
     }
 }
